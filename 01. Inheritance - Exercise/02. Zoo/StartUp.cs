2fae1d6e8f1c694ed76054d74b936a9c10019f9b
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-21 11:59:00 +0200

    My homework on topic "Inheritance" - Exercise

---
+++ 01. Inheritance - Exercise/02. Zoo/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Snake snake = new Snake("Azax");
+            Bear bear = new Bear("Karla");
+
+            Console.WriteLine(snake.Name);
+            Console.WriteLine(bear.Name);
+        }
+    }
+}
