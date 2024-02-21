4d08ddb0f46ad9b201fb10746e5fbf1a7e6af45e
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-21 12:10:23 +0200

    My homework on topic "Inheritance" - Exercise

---
+++ 01. Inheritance - Exercise/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoulMaster soulMaster = new SoulMaster("Mary", 50);
+            MuseElf museElf = new MuseElf("Ajax", 30);
+            BladeKnight bladeKnight = new BladeKnight("Fenrir", 100);
+
+
+            Console.WriteLine($"Username: {soulMaster.Username}, Level: {soulMaster.Level}");
+            Console.WriteLine($"Username: {museElf.Username}, Level: {museElf.Level}");
+            Console.WriteLine($"Username: {bladeKnight.Username}, Level: {bladeKnight.Level}");
+        }
+    }
+}
