97f05b284d39350a990e25ea1c3b3158c09a2093
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-21 11:50:31 +0200

    My homework on topic "Inheritance" - Exercise

---
+++ 01. Inheritance - Exercise/01. Person/Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age) 
+        {
+
+        }
+        public override bool IsAgeCorrect()
+        {
+            if (Age > 15)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 01. Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual bool IsAgeCorrect()
+        {
+            if (Age < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
