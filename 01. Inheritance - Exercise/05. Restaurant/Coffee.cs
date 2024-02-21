a848b7422d197709d5c381837062b8f71cb56c29
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-21 13:36:38 +0200

    My homework on topic "Inheritance" - Exercise

---
+++ 01. Inheritance - Exercise/05. Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMililiters = 50;
+        private const decimal CoffeePrice = 3.50m;
+        public double Caffeine { get;  set; }
+
+        public Coffee(string name, double caffeine) : base(name, CoffeePrice, CoffeeMililiters)
+        {
+            Caffeine = caffeine;
+        }
+    }
+}
---
+++ 01. Inheritance - Exercise/05. Restaurant/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Food food = new Food("Fish", 4.5m, 420);
+            Tea tea = new Tea("SweetTea", 2.1m, 330);
+            Soup soup = new Soup("ChickenSoup", 3.1m, 450);
+            Cake cake = new Cake("ChocolateCake");
+            Dessert dessert = new Dessert("ChocolateCake", 2.8m, 300, 800);
+            ColdBeverage coldBeverage = new ColdBeverage("GreyGoose", 10.80m, 50);
+
+            Console.WriteLine(food.Name, food.Price, food.Grams);
+            Console.WriteLine(tea.Name, tea.Price, tea.Milliliters);
+            Console.WriteLine(soup.Name, soup.Price, soup.Grams);
+            Console.WriteLine(cake.Name, cake.Price, cake.Grams);
+            Console.WriteLine(dessert.Name, dessert.Price, dessert.Grams);
+            Console.WriteLine(coldBeverage.Name, coldBeverage.Price, coldBeverage.Milliliters);
+        }
+    }
+}
