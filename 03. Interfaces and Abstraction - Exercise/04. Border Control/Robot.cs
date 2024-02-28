a8d94d5b95904f30baf22395541cb82a813411b9
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-28 12:39:30 +0200

    My homework on topic "Interfaces and Abstraction" - Exercise

---
+++ 03. Interfaces and Abstraction - Exercise/04. Border Control/Robot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderControl
+{
+    internal class Robot : IIdentifiable
+    {
+        public string Id { get; private set; }
+        public string Model { get; private set; }
+
+        public Robot(string model, string id)
+        {
+            this.Model = model;
+            this.Id = id;
+        }
+    }
+}
---
+++ 03. Interfaces and Abstraction - Exercise/04. Border Control/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using BorderControl;
+using System.Diagnostics.Metrics;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        string command = string.Empty;
+        List<IIdentifiable> ids = new List<IIdentifiable>();
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string[] commandInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            if (commandInfo.Length == 3)
+            {
+                // citizen - name, age, id
+                Citizen citizen = new Citizen(commandInfo[0], int.Parse(commandInfo[1]), commandInfo[2]);
+                ids.Add(citizen);
+            }
+            else
+            {
+                // robot - model, id
+                Robot robot = new Robot(commandInfo[0], commandInfo[1]);
+                ids.Add(robot);
+            }
+        }
+
+        string lastDigits = Console.ReadLine();
+        List<IIdentifiable> detainedEnters = new List<IIdentifiable>();
+
+        foreach (var item in ids)
+        {
+            if (item.Id.EndsWith(lastDigits))
+            {
+                detainedEnters.Add(item);
+            }
+        }
+
+        foreach (var item in detainedEnters)
+        {
+            Console.WriteLine(item.Id);
+        }
+    }
+}
