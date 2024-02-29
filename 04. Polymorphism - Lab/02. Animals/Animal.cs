c2092ebc5e9776d669926ece6d5e64a69acc35dd
Coldrain2472 <anelia.peicheva@gmail.com>
2024-02-29 13:18:14 +0200

    My homework on topic "Polymorphism" - Lab

---
+++ 04. Polymorphism - Lab/02. Animals/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string FavouriteFood { get; set; }
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return " ";
+        }
+    }
+}
