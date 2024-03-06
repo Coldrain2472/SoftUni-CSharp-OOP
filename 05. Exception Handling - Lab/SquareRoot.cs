7492587457257609b94ce09e142703c3c68ec9db
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-06 11:51:01 +0200

    My homework on topic "Exception Handling" - Lab

---
+++ 05. Exception Handling - Lab/SquareRoot.cs
@@ -0,0 +1,19 @@
+try
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number < 0)
+    {
+        throw new ArgumentException("Invalid number.");
+    }
+
+    Console.WriteLine(Math.Sqrt(number));
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Invalid number.");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
