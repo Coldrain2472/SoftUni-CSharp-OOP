3cf1eec9696199dd0bbd677f27d01fd8131dcda2
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-08 11:32:29 +0200

    My homework on topic "SOLID" - Lab

---
+++ 06. SOLID - Lab/01. Stream Progress Info/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            File musicFile = new Music("Linkin Park", "Hybrid Theory", 12, 100);
+            File pictureFile = new Picture("Dog", 3, 100);
+            StreamProgressInfo streamProgressInfo = new StreamProgressInfo(musicFile);
+            streamProgressInfo.CalculateCurrentPercent();
+        }
+    }
+}
---
+++ 06. SOLID - Lab/01. Stream Progress Info/StreamProgressInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        private File file;
+
+        public StreamProgressInfo(File file)
+        {
+            this.file = file;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.file.BytesSent * 100) / this.file.Length;
+        }
+    }
+}
