31849228e2bcabd6e6d52b5319e5f415555aea9b
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-08 11:47:30 +0200

    My homework on topic "SOLID" - Lab

---
+++ 06. SOLID - Lab/03. Detail Printer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Ivan The Programmer");
+            ICollection<string> documents = new List<string>()
+        {
+            "Document One",
+            "Document Two",
+            "Document Three"
+        };
+            Employee manager = new Manager("Sasho The Manager", documents);
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(employee);
+            employees.Add(manager);
+
+            DetailsPrinter printer = new DetailsPrinter(employees);
+            printer.PrintDetails();
+        }
+    }
+}
