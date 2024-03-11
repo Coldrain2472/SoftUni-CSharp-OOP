ae09e40b0ea2ee311c7926c9e6c412e53d232d27
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-11 11:35:31 +0200

    My homework on topic "SOLID" - Exercise

---
+++ 06. SOLID - Exercise/LogForU.Core/Appenders/ConsoleAppender.cs
@@ -0,0 +1,20 @@
+﻿using LogForU.Core.Enums;
+using LogForU.Core.Models;
+using LogForU.Core.Layouts.Interfaces;
+
+namespace LogForU.Core.Appenders;
+
+public class ConsoleAppender : Appender
+{
+    public ConsoleAppender(ILayout layout, ReportLevel reportLevel = ReportLevel.Info)
+        : base(layout, reportLevel)
+    {
+    }
+
+    public override void AppendMessage(Message message)
+    {
+        Console.WriteLine(string.Format(Layout.Format, message.CreatedTime, message.ReportLevel, message.Text));
+
+        MessagesAppended++;
+    }
+}
---
+++ 06. SOLID - Exercise/LogForU.Core/Appenders/Interfaces/IAppender.cs
@@ -0,0 +1,16 @@
+﻿using LogForU.Core.Enums;
+using LogForU.Core.Models;
+using LogForU.Core.Layouts.Interfaces;
+
+namespace LogForU.Core.Appenders.Interfaces;
+
+public interface IAppender
+{
+    ILayout Layout { get; }
+
+    ReportLevel ReportLevel { get; set; }
+
+    int MessagesAppended { get; }
+
+    void AppendMessage(Message message);
+}
