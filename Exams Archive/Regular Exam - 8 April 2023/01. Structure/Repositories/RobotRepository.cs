3eae73d229e7896925be435a4dac7655cb42dd9b
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-11 14:06:40 +0200

    Updated

---
+++ Exams Archive/Regular Exam - 8 April 2023/01. Structure/Repositories/RobotRepository.cs
@@ -0,0 +1,26 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Repositories
+{
+    public class RobotRepository : IRepository<IRobot>
+    {
+        private readonly List<IRobot> robots;
+
+        public void AddNew(IRobot model)
+        {
+            robots.Add(model);
+        }
+
+        public IRobot FindByStandard(int interfaceStandard) => robots.FirstOrDefault(x => x.InterfaceStandards.Any(y => y == interfaceStandard));
+
+        public IReadOnlyCollection<IRobot> Models() => robots;
+
+        public bool RemoveByName(string typeName) => robots.Remove(robots.FirstOrDefault(r => r.GetType().Name == typeName));
+    }
+}
