172d4aba15d40da010ea641c79896b257ad5a893
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-11 13:04:31 +0200

    Updated

---
+++ Exams Archive/Regular Exam - 5 August 2023/01. Structure/Models/StudentLoan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Models
+{
+    public class StudentLoan : Loan
+    {
+        private const int defaultInterestRate = 1;
+        private const double defaultAmount = 10_000;
+
+        public StudentLoan() : base(defaultInterestRate, defaultAmount) { }
+    }
+}
