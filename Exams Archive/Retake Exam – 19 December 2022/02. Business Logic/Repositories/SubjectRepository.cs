f6fb850873294a8120016de6282a46fe381364b9
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-13 14:07:43 +0200

    Updated

---
+++ Exams Archive/Retake Exam – 19 December 2022/02. Business Logic/Repositories/SubjectRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories;
+
+public class SubjectRepository : IRepository<ISubject>
+{
+    private List<ISubject> subjects;
+
+    public SubjectRepository()
+    {
+        subjects = new List<ISubject>();
+    }
+
+    public IReadOnlyCollection<ISubject> Models => subjects;
+
+    public void AddModel(ISubject model) => subjects.Add(model);
+
+    public ISubject FindById(int id) => subjects.FirstOrDefault(s => s.Id == id);
+
+    public ISubject FindByName(string name) => subjects.FirstOrDefault(s => s.Name == name);
+}
