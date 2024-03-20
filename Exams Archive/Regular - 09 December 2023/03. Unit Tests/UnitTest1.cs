12ded6585857e1b88f13e0b0cc070cde419714b5
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-20 11:57:40 +0200

    Updated

--- Exams Archive/Regular - 09 December 2023/03. Unit Tests/UnitTest1.cs
+++ Exams Archive/Regular - 09 December 2023/03. Unit Tests/UnitTest1.cs
@@ -2,153 +2,108 @@ namespace Railway.Tests
 {
     using NUnit.Framework;
     using System;
-    public class RailwayStationTests
+    using System.Linq;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+    public class Tests
     {
         private RailwayStation station;
-        private string name = "Hogwarts";
-        private string train1 = "London-Dublin";
-        private string train2 = "London-Hogwarts";
-        private string train3 = "London-Hogsmead";
 
         [SetUp]
-        public void SetUp()
+        public void Setup()
         {
-            station = new RailwayStation(name);
+            station = new RailwayStation("Test Station");
         }
 
         [Test]
-        public void ConstructorShouldInitializeStation()
+        public void TrainHasArrivedShouldRemoveTrainFromArrivalTrainList()
         {
-            Assert.IsNotNull(station);
+            station.NewArrivalOnBoard("TrainOne");
+            station.TrainHasArrived("TrainOne");
+
+            Assert.That(!station.ArrivalTrains.Contains("TrainOne"));
+            Assert.AreEqual(0, station.ArrivalTrains.Count);
+        }
+
+
+        [Test]
+        public void TrainHasArrivedWhenTrainIsNextReturnsExpectedMessage()
+        {
+            // Arrange
+            station.NewArrivalOnBoard("Train A");
+
+            // Act
+            var result = station.TrainHasArrived("Train A");
+
+            // Assert
+            Assert.AreEqual("Train A is on the platform and will leave in 5 minutes.", result);
         }
 
         [Test]
-        public void ConstructorShouldSetNameCorrectly()
+        public void TrainHasArrivedWhenOtherTrainsAreBeforeReturnsExpectedMessage()
         {
-            Assert.AreEqual(name, station.Name);
+            // Arrange
+            station.NewArrivalOnBoard("Train A");
+            station.NewArrivalOnBoard("Train B");
+
+            // Act
+            var result = station.TrainHasArrived("Train B");
+
+            // Assert
+            Assert.AreEqual("There are other trains to arrive before Train B.", result);
         }
 
         [Test]
-        public void ConstructorShouldInitializeArrivalTrains()
+        public void TrainHasLeftShouldRemoveTrainFromDepartureTrainList()
         {
-            Assert.IsNotNull(station.ArrivalTrains);
+            station.NewArrivalOnBoard("FirstTrain");
+            station.NewArrivalOnBoard("SecondTrain");
+            station.TrainHasArrived("FirstTrain");
+            station.TrainHasArrived("SecondTrain");
+            station.TrainHasLeft("FirstTrain");
+
+            Assert.IsFalse(station.DepartureTrains.Contains("FirstTrain"));
         }
 
         [Test]
-        public void ConstructorShouldInitializeDepartureTrains()
+        public void TrainHasLeftWhenTrainHasLeftReturnsTrue()
         {
-            Assert.IsNotNull(station.DepartureTrains);
+            // Arrange
+            station.NewArrivalOnBoard("Train A");
+            station.TrainHasArrived("Train A");
+
+            // Act
+            var result = station.TrainHasLeft("Train A");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TrainHasLeftWhenTrainHasNotLeftReturnsFalse()
+        {
+            // Arrange
+            station.NewArrivalOnBoard("Train A");
+            station.TrainHasArrived("Train A");
+
+            // Act
+            var result = station.TrainHasLeft("Train B");
+
+            // Assert
+            Assert.IsFalse(result);
         }
 
         [TestCase(null)]
         [TestCase("")]
         [TestCase(" ")]
         [TestCase("\t")]
-
-        public void SettingTheNameToNullOrWhitespaceShouldThrowException(string invalidName)
+        public void RailwayStationConstructorNameThrowsArgumentException(string name)
         {
-            ArgumentException exception = Assert.Throws<ArgumentException>(()
-                => station = new RailwayStation(invalidName));
-
-            Assert.AreEqual("Name cannot be null or empty!", exception.Message);
-        }
-
-        [Test]
-        public void NewArrivalOnBoardShouldCorrectlyEnqueueTrainOnArrivalTrains()
-        {
-            Assert.AreEqual(0, station.ArrivalTrains.Count);
-            station.NewArrivalOnBoard(train1);
-            Assert.AreEqual(1, station.ArrivalTrains.Count);
-            Assert.AreEqual(train1, station.ArrivalTrains.Peek());
-        }
-
-
-        [Test]
-        public void TrainHasArrivedShouldRemoveTrainFromArrivalTrainList()
-        {
-            station.NewArrivalOnBoard(train1);
-            station.TrainHasArrived(train1);
-
-            Assert.That(!station.ArrivalTrains.Contains(train1));
-            Assert.AreEqual(0, station.ArrivalTrains.Count);
-        }
-
-        [Test]
-        public void TrainHasArrivedShouldAddTrainToDepartureTrainList()
-        {
-            station.NewArrivalOnBoard(train1);
-            Assert.AreEqual(0, station.DepartureTrains.Count);
-            station.TrainHasArrived(train1);
-
-            Assert.AreEqual(train1, station.DepartureTrains.Peek());
-            Assert.AreEqual(1, station.DepartureTrains.Count);
-        }
-
-        [Test]
-        public void TrainHasArrivedShouldReturnCorrectMessageAboutDeparturingTrain()
-        {
-            station.NewArrivalOnBoard(train1);
-
-            string expectedResult = $"{train1} is on the platform and will leave in 5 minutes.";
-            Assert.AreEqual(expectedResult, station.TrainHasArrived(train1));
-        }
-
-        [Test]
-        public void TrainHasArrivedShouldReturnCorrectMessageIfThereAreMoreArrivingTrains()
-        {
-            station.NewArrivalOnBoard(train1);
-            station.NewArrivalOnBoard(train2);
-            station.NewArrivalOnBoard(train3);
-
-            string expectedResult = $"There are other trains to arrive before {train3}.";
-            Assert.AreEqual(expectedResult, station.TrainHasArrived(train3));
-        }
-
-        [Test]
-        public void TrainHasArrivedShouldRemoveFirstFromArrivalTrainsAndQueueToDepartureTrains()
-        {
-            station.NewArrivalOnBoard(train1);
-            station.NewArrivalOnBoard(train2);
-            station.TrainHasArrived(train1);
-
-            Assert.AreEqual(false, station.ArrivalTrains.Contains(train1));
-            Assert.AreEqual(train2, station.ArrivalTrains.Peek());
-            Assert.AreEqual(true, station.DepartureTrains.Contains(train1));
-            Assert.AreEqual(train1, station.DepartureTrains.Peek());
-        }
-
-        [Test]
-        public void TrainHasLeftShouldReturnTrueWhenTrainIsTheOneToLeave()
-        {
-            station.NewArrivalOnBoard(train1);
-            station.NewArrivalOnBoard(train2);
-            station.TrainHasArrived(train1);
-            station.TrainHasArrived(train2);
-
-            Assert.AreEqual(true, station.TrainHasLeft(train1));
-        }
-
-        [Test]
-        public void TrainHasLeftShouldRemoveTrainFromDepartureTrainList()
-        {
-            station.NewArrivalOnBoard(train1);
-            station.NewArrivalOnBoard(train2);
-            station.TrainHasArrived(train1);
-            station.TrainHasArrived(train2);
-            station.TrainHasLeft(train1);
-
-            Assert.IsFalse(station.DepartureTrains.Contains(train1));
-        }
-
-        [Test]
-        public void TrainHasLeftShouldReturnFalseWhenTrainIsNotTheOneToLeave()
-        {
-            station.NewArrivalOnBoard(train1);
-            station.NewArrivalOnBoard(train2);
-            station.TrainHasArrived(train1);
-            station.TrainHasArrived(train2);
-
-            Assert.AreEqual(false, station.TrainHasLeft(train2));
+            // Arrange, Act & Assert
+            Assert.Throws<ArgumentException>(() => new RailwayStation(null));
+            Assert.Throws<ArgumentException>(() => new RailwayStation(""));
+            Assert.Throws<ArgumentException>(() => new RailwayStation("   "));
         }
     }
 }
