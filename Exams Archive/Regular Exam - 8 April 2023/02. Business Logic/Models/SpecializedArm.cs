5f97b9a25904b9bdafe610c132f8d574226365e1
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-03-26 12:55:21 +0200

    Updated

--- Exams Archive/Regular Exam - 8 April 2023/02. Business Logic/Models/SpecializedArm.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RobotService.Models;
-
-internal class SpecializedArm : Supplement
-{
-    private const int interfaceStandard = 10045;
-    private const int batteryUsage = 10000;
-
-    public SpecializedArm() : base(interfaceStandard, batteryUsage){}
-}
