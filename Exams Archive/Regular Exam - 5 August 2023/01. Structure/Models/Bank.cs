8af923564a39a09f9277b221b370949e7093a5f8
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-04-03 10:57:22 +0300

    Updated

---
+++ Exams Archive/Regular Exam - 5 August 2023/01. Structure/Models/Bank.cs
@@ -0,0 +1,76 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Models
+{
+    public abstract class Bank : IBank
+    {
+        private string name;
+        private readonly List<ILoan> loans;
+        private readonly List<IClient> clients;
+
+        public Bank(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            loans = new List<ILoan>();
+            clients = new List<IClient>();
+        }
+
+        public string Name
+        {
+            get=>name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.BankNameNullOrWhiteSpace);
+                }
+                name = value;
+            }
+        }
+
+        public int Capacity {get;private set; }
+
+        public IReadOnlyCollection<ILoan> Loans => loans;
+
+        public IReadOnlyCollection<IClient> Clients => clients;
+
+        public void AddClient(IClient Client)
+        {
+            if (Capacity <= clients.Count)
+            {
+                throw new ArgumentException(ExceptionMessages.NotEnoughCapacity);
+            }
+            clients.Add(Client);
+        }
+
+        public void AddLoan(ILoan loan)=>loans.Add(loan);
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {Name}, Type: {GetType().Name}");
+            if (clients.Count == 0)
+            {
+                sb.AppendLine("Clients: none");
+            }
+            else
+            {
+                sb.AppendLine($"Clients: {string.Join(", ", clients)}");
+            }
+            sb.AppendLine($"Loans: {loans.Count}, Sum of Rates: {SumRates()}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void RemoveClient(IClient Client)=>clients.Remove(Client);
+
+        public double SumRates()=>loans.Sum(x=>x.InterestRate);
+    }
+}
